a6924daef85261fc4a718e002e9eebb3c93a7d36
DelsantiV <vincent.delsanti@gmail.com>
2026-08-01 10:49:24 +0200

    Update BirdsManager.cs
    Reset if all birds have been used

--- Assets/Scripts/Game/Managers/BirdsManager.cs
+++ Assets/Scripts/Game/Managers/BirdsManager.cs
@@ -10,20 +10,35 @@ namespace BirdExpert
         public static List<string> spCodesList { get => allBirds.Keys.ToList(); }
         public static List<string> GetAllNamesInLang(Lang lang) => allBirds.Values.Select(bird => bird.GetName(lang)).ToList();
         private List<string> goodBirdsCodes;
+        private List<string> selectedBirdsCodes;
         public BirdInfo GetBirdFromCode(string spCode)
         {
             return allBirds[spCode];
         }
         public BirdInfo GetBirdFromLang(Lang lang, string name)
         {
-            return allBirds.Values.ToList().Find(bird => bird.GetName(lang) == name);
+            BirdInfo bird = allBirds.Values.ToList().Find(bird => bird.GetName(lang) == name);
+            if (bird == null)
+            {
+                Debug.LogWarning("Bird " + name + " (in language " + lang + ") was not found !");
+            }
+            return bird;
         }
-        public BirdInfo GetRandomBird(bool skipBirds = true)
+        public BirdInfo GetRandomBird()
         {
+            if (goodBirdsCodes.Count == 0)
+            {
+                goodBirdsCodes = selectedBirdsCodes;
+                Debug.Log("Used all birds for this quiz, reinitializing selection");
+            }
             string birdCode = goodBirdsCodes[Random.Range(0, goodBirdsCodes.Count())];
             return GetBirdFromCode(birdCode);
         }
         public void AddBirdToSkip(BirdInfo bird) => goodBirdsCodes.Remove(bird.spCode);
-        public void InitGoodBirds() => goodBirdsCodes = spCodesList; // Modify to include predicates based on game mode choice
+        public void InitGoodBirds() 
+        {
+            selectedBirdsCodes = spCodesList; // Modify to include predicates based on game mode choice
+            goodBirdsCodes = selectedBirdsCodes;
+        }
     }
 }
